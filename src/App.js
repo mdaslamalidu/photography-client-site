e7490b203b8943aad5cb51717a3e1050f6d3017c
Md Aslam Ali <mdaslamali675@gmail.com>
2022-11-08 12:18:26 +0600

    setup file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./Routes/Routers";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello</h2>
+    <div>
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
