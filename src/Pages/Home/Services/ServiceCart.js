202064224ef774fc0f65af00dec590401c3cd501
Md Aslam Ali <mdaslamali675@gmail.com>
2022-11-08 15:14:17 +0600

    add service

---
+++ src/Pages/Home/Services/ServiceCart.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const ServiceCart = ({ service }) => {
+  const { img, _id, desc, name } = service;
+  return (
+    <div>
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div>
+          <div className="overflow-hidden transition-shadow duration-300 bg-white rounded shadow-sm">
+            <img src={img} className="object-cover w-full h-64" alt="" />
+            <div className="p-5 border border-t-0">
+              <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
+                <a
+                  href="/"
+                  className="transition-colors duration-200 text-blue-gray-900 hover:text-deep-purple-accent-700"
+                  aria-label="Category"
+                  title="traveling"
+                >
+                  traveling
+                </a>
+                <span className="text-gray-600">— 28 Dec 2020</span>
+              </p>
+              <a
+                href="/"
+                aria-label="Category"
+                title="Visit the East"
+                className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-deep-purple-accent-700"
+              >
+                {name}
+              </a>
+              <p className="mb-2 text-gray-700">{desc.slice(0, 99) + "...."}</p>
+              <a
+                href="/"
+                aria-label=""
+                className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+              >
+                Learn more
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceCart;
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ServiceCart from "./ServiceCart";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/services")
+      .then((res) => res.json())
+      .then((data) => setServices(data))
+      .catch((err) => console.error(err));
+  }, []);
   return (
     <div>
-      <h2>Services section</h2>
+      <h2>My Services</h2>
+      <div className="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+        {services.map((service) => (
+          <ServiceCart key={service._id} service={service}></ServiceCart>
+        ))}
+      </div>
+      <Link>
+        <button>See All</button>
+      </Link>
     </div>
   );
 };
