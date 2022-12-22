791a3a3540c22e726b2daf4702f5647066be5d51
mdaslamalidu <thehometutor2022@gmail.com>
2022-12-22 17:19:53 +0600

    update button

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -40,21 +40,20 @@ const Banner = () => {
               viewBox="0 0 1160 163"
             >
               <path
-                fill="currentColor"
+                fill="#111827"
                 d="M-164 13L-104 39.7C-44 66 76 120 196 141C316 162 436 152 556 119.7C676 88 796 34 916 13C1036 -8 1156 2 1216 7.7L1276 13V162.5H1216C1156 162.5 1036 162.5 916 162.5C796 162.5 676 162.5 556 162.5C436 162.5 316 162.5 196 162.5C76 162.5 -44 162.5 -104 162.5H-164V13Z"
               />
             </svg>
             <div className="relative px-4 py-16 mx-auto overflow-hidden sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
               <div className="flex flex-col items-center justify-between xl:flex-row">
                 <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
-                  <h2 className="max-w-lg mb-6 font-sans md:text-6xl text-white font-bold tracking-tight  text-4xl sm:leading-none">
+                  <h2 className="max-w-lg mb-6 font-sans md:text-6xl text-gray font-bold tracking-tight  text-4xl sm:leading-none">
                     Welcome To My <br className="hidden md:block" />
                     Photography Studio
                   </h2>
                   <Link
                     to="/services"
-                    aria-label=""
-                    className="inline-flex items-center font-semibold tracking-wider transition-colors duration-200  text-white hover:text-700 py-3 px-3 rounded  border-2"
+                    className="text-xl border-2 rounded py-3 px-5 transition text-white hover:bg-[#303640] hover:border-[#303640]"
                   >
                     My Services
                     <svg
@@ -74,7 +73,7 @@ const Banner = () => {
       <SwiperSlide>
         <div className="relative ">
           <img
-            src="https://www.nerdscompany.com/wp-content/uploads/2018/05/digital_photography_banner.jpg"
+            src="https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/how-to-become-a-professional-photographer-1001.jpg?fit=1500%2C1000&ssl=1"
             className="absolute inset-0 object-cover w-full h-full"
             alt=""
           />
@@ -84,21 +83,20 @@ const Banner = () => {
               viewBox="0 0 1160 163"
             >
               <path
-                fill="currentColor"
+                fill="#111827"
                 d="M-164 13L-104 39.7C-44 66 76 120 196 141C316 162 436 152 556 119.7C676 88 796 34 916 13C1036 -8 1156 2 1216 7.7L1276 13V162.5H1216C1156 162.5 1036 162.5 916 162.5C796 162.5 676 162.5 556 162.5C436 162.5 316 162.5 196 162.5C76 162.5 -44 162.5 -104 162.5H-164V13Z"
               />
             </svg>
             <div className="relative px-4 py-16 mx-auto overflow-hidden sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
               <div className="flex flex-col items-center justify-between xl:flex-row">
                 <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
-                  <h2 className="max-w-lg mb-6 font-sans md:text-6xl text-white font-bold tracking-tight  text-4xl sm:leading-none">
+                  <h2 className="max-w-lg mb-8 font-sans md:text-6xl text-gray font-bold tracking-tight  text-4xl sm:leading-none">
                     Welcome To My <br className="hidden md:block" />
                     Photography Studio
                   </h2>
                   <Link
                     to="/services"
-                    aria-label=""
-                    className="inline-flex items-center font-semibold tracking-wider transition-colors duration-200  text-white hover:text-700 py-3 px-3 rounded  border-2"
+                    className="text-xl border-2 rounded py-3 px-5 transition text-white hover:bg-[#303640] hover:border-[#303640]"
                   >
                     My Services
                     <svg
